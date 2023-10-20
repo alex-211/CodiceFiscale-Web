0cf57bcf169f9abc3cde4a4901eaa31f032b5b07
Alessandro Porpiglia <alessandro.porpiglia@itismajo.onmicrosoft.com>
2023-10-20 12:06:01 +0200

    First commit
    Apparentemente questo sarà un casino. usati metodi non esistenti per il cognome, fixare

---
+++ script.js
@@ -0,0 +1,16 @@
+function calcola() {
+    let codice = [];
+
+    let cognome = document.dati;
+    let nome;
+    let meseNascita;
+    let annoNascita;
+    let ggNascita
+    let sesso;
+    let comuneNascita;
+    let controllo;
+
+    // qua le verifiche della correttezza dei dati inviati
+
+
+}
